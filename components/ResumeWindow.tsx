5f01546ebfabd381cd88a8e3291fa7bca7bea8cf
Ruhaan Bhargav <ruhaanbhargav6@gmail.com>
2026-03-17 14:36:35 -0400

    Made the SVG icons match on both the icons

--- components/ResumeWindow.tsx
+++ components/ResumeWindow.tsx
@@ -151,10 +151,9 @@ export default function ResumeWindow({ onClose }: { onClose: () => void }) {
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                 <polyline points="14 2 14 8 20 8"></polyline>
-                <path d="M8 13h2"></path>
-                <path d="M8 17h2"></path>
-                <path d="M14 13h2"></path>
-                <path d="M14 17h2"></path>
+                <line x1="16" y1="13" x2="8" y2="13"></line>
+                <line x1="16" y1="17" x2="8" y2="17"></line>
+                <polyline points="10 9 9 9 8 9"></polyline>
               </svg>
             </div>
 
